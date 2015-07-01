322506dcaaea1e684b338ecbcbc93540e36c72b5
lingxiang.wang <lingxiang.wang@alibaba-inc.com>
2015-07-01 12:08:14 +0800

    Demo7——如何使用Robolectric进行单元测试

---
+++ Demo7——如何使用Robolectric进行单元测试/android-studio-robolectric-example/app/src/test/java/com/example/joshskeen/myapplication/MyActivityTest.java
@@ -0,0 +1,44 @@
+package com.example.joshskeen.myapplication;
+
+import com.squareup.fest.BuildConfig;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.fest.assertions.api.ANDROID.assertThat;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 21)
+public class MyActivityTest {
+
+    private MyActivity mActivity;
+
+    @Before
+    public void setup() {
+        mActivity = Robolectric.buildActivity(MyActivity.class).create().get();
+    }
+
+    @Test
+    public void myActivityAppearsAsExpectedInitially() {
+        assertThat(mActivity.mClickMeButton).hasText("Click me!");
+        assertThat(mActivity.mHelloWorldTextView).hasText("Hello world!");
+    }
+
+    @Test
+    public void clickingClickMeButtonChangesHelloWorldText() {
+        assertThat(mActivity.mHelloWorldTextView).hasText("Hello world!");
+        mActivity.mClickMeButton.performClick();
+        assertThat(mActivity.mHelloWorldTextView).hasText("HEY WORLD");
+    }
+
+	@Test
+	public void clickingClickMeButtonChangesHelloWorldTextShouldBeFailed() {
+		assertThat(mActivity.mHelloWorldTextView).hasText("Hello world!");
+		mActivity.mClickMeButton.performClick();
+		assertThat(mActivity.mHelloWorldTextView).hasText("HEY");
+	}
+}
